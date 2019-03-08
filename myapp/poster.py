6abf8547e9ce0226d5f67f893c04d0232a24d5c4
Fujian Wang <zhangkehao12@outlook.com>
2019-03-08 23:19:40 +0800

    宏宏帮我写了一段模拟范宇豪前端测试的代码

---
+++ myapp/poster.py
@@ -0,0 +1,13 @@
+import requests;
+
+data = {
+    'action': 'login',
+    'username': '范宇豪',
+    'password':'123',
+    'r_username': '我叫永远的王',
+    'r_password': '123'
+}
+
+r = requests.get('http://localhost:8080/index', params=data)
+
+print(r.text)
